4e4580ebe448950d2abbe09d3cc933f713b6650b
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-25 10:24:58 -0400

    Adds get route

--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { registerUser, loginUser } from '../../controllers/authController.js';
+import User from '../../models/User.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
 
 export default router;
